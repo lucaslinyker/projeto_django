84a4c96adc99fbb466f80b8462c28543814ecffc
lucaslinyker <lucaslinyker@outlook.com>
2025-04-01 08:29:49 -0300

    fix: arrumar tabela filme

--- filmes/admin.py
+++ filmes/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from .models import Filme
 
 # Register your models here.
 class FilmeAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'descricao', 'ano_lancamento', 'duracao')
     search_fields = ('nome', 'descricao')
+
+admin.site.register(Filme, FilmeAdmin)
