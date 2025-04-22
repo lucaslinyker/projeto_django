78db829198e724da6b06eab2de9f649982fa986c
lucaslinyker <lucaslinyker@outlook.com>
2025-04-22 07:38:56 -0300

    feat: adicionar genero aos filmes

--- filmes/admin.py
+++ filmes/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Filme
+from .models import Filme, Genero
 
 # Register your models here.
 class FilmeAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'descricao', 'ano_lancamento', 'duracao')
     search_fields = ('nome', 'descricao')
-
 admin.site.register(Filme, FilmeAdmin)
+
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    search_fields = ('nome',)
+admin.site.register(Genero, GeneroAdmin)
--- filmes/models.py
+++ filmes/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Genero(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
 # Create your models here.
 class Filme(models.Model):
     id = models.AutoField(primary_key=True)
@@ -7,7 +14,8 @@ class Filme(models.Model):
     descricao = models.TextField()
     ano_lancamento = models.IntegerField()
     duracao = models.IntegerField()
-    genero = models.CharField(max_length=50)
+    genero = models.ForeignKey(Genero, on_delete=models.PROTECT, related_name='filmes')
+    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
 
     def __str__(self):
         return self.nome
