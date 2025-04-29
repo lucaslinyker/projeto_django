591623fccf5c1f415b55e1dec50e6f9787885347
lucaslinyker <lucaslinyker@outlook.com>
2025-04-29 07:47:34 -0300

    feat: adicionar upload de arquivos
    upload de imagens

--- core/settings.py
+++ core/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MIDIA_ROOT = os.path.join(BASE_DIR, 'midia')
+MIDIA_URL = '/midia/'
