15db32aae61efb36ad4b6be5b3adf75940cbce9a
lucaslinyker <lucaslinyker@outlook.com>
2025-05-27 08:28:49 -0300

    feat: adicionar templates e url

--- core/urls.py
+++ core/urls.py
@@ -1,26 +1,20 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from filmes.views import filme_view
+from filmes.views import FilmesListView, NovoFilmeCreateView, FilmeDetailView
+from filmes.views import FilmesListView, FilmeDeleteView
+from usuarios.views import usuario_view, login_view, logout_view
 
 urlpatterns = [
+    path('', FilmesListView.as_view(), name='filmes_list'),
+    path('users/', usuario_view, name='usuarios'),
     path('admin/', admin.site.urls),
-    path('filmes/', filme_view),
+    path('filmes/', FilmesListView.as_view(), name='filmes_list'),
+    path('novo_filme/', NovoFilmeCreateView.as_view(), name='novo_filme'),
+    path('filme/<int:pk/>', FilmeDetailView.as_view(), name='filme_detail'),
+    path('filme/<int:pk>/', FilmeDetailView.as_view(), name='filme_detail'),
+    path('filme/delete/<int:pk>/', FilmeDeleteView.as_view(), name='filme_delete'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ] + static(settings.MIDIA_URL, document_root=settings.MIDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- filmes/views.py
+++ filmes/views.py
@@ -1,11 +1,49 @@
-from django.shortcuts import render
 from filmes.models import Filme
+from filmes.forms import FilmesModelForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, CreateView, DetailView
+from django.viens.generic import UpdateView, DeleteView
 
-# Create your views here.
-def filme_view(request):
-    # filmes = Filme .objects.all()
-    search = request.GET.get('search')
-    if search:
-        filmes = Filme.objects.filter(nome__contains=search)
+class FilmesListView(ListView):
+    model = Filme
+    template_name = 'filmes.html'
+    context_object_name = 'filmes'
 
-    return render(request, 'filmes.html', {'filmes': filmes})
+    def get_queryset(self):
+        filmes = super().get_queryset().order_by('titulo')
+        search = self.request.GET.get('search')
+        if search:
+            filmes = filmes.filter(titulo__icontains=search)
+        return filmes
+    
+class FilmeDetailView(DetailView):
+    model = Filme
+    template_name = 'filme_detail.html'
+
+@method_decorator(login_required(login_url='login'),
+                    name='dispatch')
+class NovoFilmeCreateView(CreateView):
+    model = FilmesListView
+    form_class = FilmesModelForm
+    template_name = 'novo_filme.html'
+    success_url = '/filmes/'
+
+@method_decorator(login_required(login_url='login'),
+                    name='dispatch')
+class FilmeUpdateView(UpdateView):
+    model = Filme
+    form_class = FilmesModelForm
+    template_name = 'filme_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('filme_detail',
+                                kwargs={'pk': self.object.pk})
+    
+@method_decorator(login_required(login_url='login'),
+                    name='dispatch')
+class FilmeDeleteView(DeleteView):
+    model = Filme
+    template_name = 'filme_delete.html'
+    success_url = '/filmes/'
