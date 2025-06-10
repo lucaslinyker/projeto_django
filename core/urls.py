f4c558f41357cc6a9622b59e512405ee57c7b92c
unset <unset>
2025-06-10 08:33:15 -0300

    adicionar templates para usuarios

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from filmes.views import FilmesListView, NovoFilmeCreateView, FilmeDetailView
-from filmes.views import FilmesListView, FilmeDeleteView
+from filmes.views import FilmeUpdateView, FilmeDeleteView
 from usuarios.views import usuario_view, login_view, logout_view
 
 urlpatterns = [
@@ -12,9 +12,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('filmes/', FilmesListView.as_view(), name='filmes_list'),
     path('novo_filme/', NovoFilmeCreateView.as_view(), name='novo_filme'),
-    path('filme/<int:pk/>', FilmeDetailView.as_view(), name='filme_detail'),
     path('filme/<int:pk>/', FilmeDetailView.as_view(), name='filme_detail'),
-    path('filme/delete/<int:pk>/', FilmeDeleteView.as_view(), name='filme_delete'),
+    path('filme/<int:pk>/update/', FilmeUpdateView.as_view(), name='filme_detail'),
+    path('filme/<int:pk>/delete/', FilmeDeleteView.as_view(), name='filme_delete'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 ] + static(settings.MIDIA_URL, document_root=settings.MIDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
